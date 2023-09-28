aaf883f4a9afdc1fac98d0b54851e54e158cad1d
TahsinMostafiz <mostafizur.webdesigner131@gmail.com>
2023-09-28 12:12:02 +0600

    Stats Section Design Complete

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,5 @@
 import HeroBg from "@/components/HeroBg";
+import Stats from "@/components/Stats";
 import WorkList from "@/components/WorkList";
 import React from "react";
 
@@ -7,6 +8,7 @@ const Home = () => {
     <div>
       <HeroBg />
       <WorkList />
+      <Stats />
     </div>
   );
 };
