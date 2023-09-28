33c21e5057c3f88771282f7878a1a571146a23b5
TahsinMostafiz <mostafizur.webdesigner131@gmail.com>
2023-09-28 11:31:21 +0600

    Work List Section Design Complete

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,12 @@
 import HeroBg from "@/components/HeroBg";
+import WorkList from "@/components/WorkList";
 import React from "react";
 
 const Home = () => {
   return (
     <div>
       <HeroBg />
+      <WorkList />
     </div>
   );
 };
